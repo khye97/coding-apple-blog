f01b3bf2cea057c3fa6055fd4c5a249603f7a22f
Hyejin Kim <khyejin08@naver.com>
2024-07-30 21:13:23 +0900

    4강 state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let post = "강남 우동 맛집";
+  //let post = "강남 우동 맛집";
+  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
 
   return (
     <div className="App">
       <div className='gray-nav'>
-        <h4 style={{fontSize:'20px'}}>블로그</h4>
+        <h4 style={{fontSize:'20px'}}>Blog</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{title[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
