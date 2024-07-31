4b216b54991a840b7636bd9ad20b37f62cb2f2de
Hyejin Kim <khyejin08@naver.com>
2024-07-31 19:06:13 +0900

    모달창 띄우기

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-  let [like, setLike] = useState(0);
+  let [like, setLike] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false);
 
   // function 함수 (){
   //   setLike(like + 1);
@@ -22,6 +23,17 @@ function App() {
     setTitle(sortArray)
   }
 
+  function showModal(){
+    modal == true ? setModal(false) : setModal(true);
+  }
+
+  function countLike (event){
+    let copy = [...like];
+    let id = event.target.id;
+    copy[id] = copy[id] + 1;
+    setLike(copy);
+  }
+
   return (
     <div className="App">
       <div className='gray-nav'>
@@ -29,8 +41,9 @@ function App() {
       </div>
       <button onClick={sort}>가나다순 정렬</button>
       <button onClick={changeTitle}>변경</button>
+      {/*
       <div className="list">
-        <h4>{title[0]} <span onClick={() => { setLike(like + 1) }}>❤️</span> {like}</h4>
+        <h4>{title[0]} <span onClick={countLike}>❤️</span> {like[0]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
@@ -38,9 +51,47 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{title[2]}</h4>
+        <h4 onClick={() => { setModal(!modal)}}>{title[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div>*/}
+  {/*}
+      {title.map((item, index)=> {
+        return (
+          <div className="list" key={index}>
+            <h4 onClick={() => { setModal(!modal) }}>{item}</h4>
+            <span onClick={()=> {
+              let copy = [...like];
+              copy[index] = copy[index] + 1;
+              setLike(copy);
+            }}>❤️</span> {like[index]}
+            <p>2월 17일 발행</p>
+          </div>
+        )
+      })} */}
+
+      {title.map((item, index)=> {
+        return (
+          <div className="list" key={index}>
+            <h4 onClick={() => { setModal(!modal) }}>{item}</h4>
+            <span id={index} onClick={countLike}>❤️</span> {like[index]}
+            <p>2월 17일 발행</p>
+          </div>
+        )
+      })}
+
+      {modal == true ? <Modal title={title} changeTitle={changeTitle} /> : null}
+      
+    </div>
+  );
+}
+
+function Modal (props){
+  return (
+    <div className="modal">
+      <h4>{props.title[0]}</h4>
+      <p>날짜</p>
+      <p>상세 내용</p>
+      <button onClick={props.changeTitle}>글 수정</button>
     </div>
   );
 }
